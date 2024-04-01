9ce8781fc8df621db9754fb9560a26f4858de1b6
carter-sw <6k2hbc@gmail.com>
2024-04-01 20:26:55 +0900

    4.1

---
+++ src/main/java/com/example/be2ndproject/shopping_mall/repository/space/SpaceJpaRepository.java
@@ -0,0 +1,16 @@
+package com.example.be2ndproject.shopping_mall.repository.Space;
+
+import com.example.be2ndproject.shopping_mall.repository.member.Member;
+import com.example.be2ndproject.shopping_mall.repository.review.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SpaceJpaRepository extends JpaRepository<Space,Integer> {
+
+
+    void deleteByMember(Optional<Member> member);
+
+}
